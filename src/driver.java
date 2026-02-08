754e7f54e797b273f3e5b641bcdbef0cc02f8ec9
wontzer <wontzer@gmail.com>
2026-02-07 19:36:18 -0500

    Runability Updates
    Removed logging and stripped out all runtime arguments

--- src/driver.java
+++ src/driver.java
@@ -12,12 +12,8 @@ public class driver {
 	 * @param String imgDir
 	 */
 	public static void main(String[] args) {
-		if(args.length != 1){
-			System.out.println("java driver <imgDir>");
-			System.exit(0);
-		}
-		
-		Properties properties = new Properties(args[0]);
+		String currentDir = System.getProperty("user.dir");
+		Properties properties = new Properties(currentDir);
 		Board board = new Board(properties);
 		board.init();
 	}
